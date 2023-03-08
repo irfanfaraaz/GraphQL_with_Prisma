907899f9d83a9214d043a238b776802d836d8d1c
irfan <irfanfaraaz03@gmail.com>
2023-03-08 13:00:16 +0530

    made changes

--- src/resolvers.js
+++ src/resolvers.js
@@ -25,25 +25,25 @@ const resolvers = {
         PackageList.push(package);
         return package;
     },
-    updatePackage: (parent, args) => {
-        // const id = args.input.id;
-        // const package = args.input;
-        // PackageList.forEach((package)=>{
-        //     if(package.id===id)
-        //         Object.assign(package, args.input);
-        // })
+    // updatePackage: (parent, args) => {
+    //     // const id = args.input.id;
+    //     // const package = args.input;
+    //     // PackageList.forEach((package)=>{
+    //     //     if(package.id===id)
+    //     //         Object.assign(package, args.input);
+    //     // })
         
-        // PackageList.push(package);
-        // return package;
-        const id = args.input.id;
-        const package = args.input;
-        const index = PackageList.findIndex((package)=>package.id===id);
-        if(index!==-1){
-            PackageList[index]=package;
-        }
-        return package;
+    //     // PackageList.push(package);
+    //     // return package;
+    //     const id = args.input.id;
+    //     const package = args.input;
+    //     const index = PackageList.findIndex((package)=>package.id===id);
+    //     if(index!==-1){
+    //         PackageList[index]=package;
+    //     }
+    //     return package;
         
-    },
+    // },
     deletePackage: (parent, args) => {
         const id = args.id;
         _.remove(PackageList, (package)=>package.id===id);
